efa370399d9e6d7267410d4f43f76d3b568379cb
JarmoA92 <67738499+JarmoA92@users.noreply.github.com>
2021-05-06 20:55:18 +0300

    lossson

---
+++ structure/js/main.js
@@ -0,0 +1,25 @@
+//('') - string
+
+let name = 'Timur'
+console.log('My name :', name)
+
+console.log(typeof name)
+console.log(typeof 10)
+console.log(typeof true) //false
+console.log(typeof undefined) // empty
+console.log(typeof [])
+console.log(typeof {})
+
+name = 10
+
+
+
+console.log('result' , name + 25) // concatenation
+
+let str ='text'
+
+console.log('result' , str + 25 , 25 + str)
+
+
+let cars = ['audi',10,true,name,undefined]
+
