3fea60ca33015f263b8beff9693992ba0f58e82f
JarmoA92 <67738499+JarmoA92@users.noreply.github.com>
2021-05-11 20:52:19 +0300

    urok

--- structure/js/main.js
+++ structure/js/main.js
@@ -14,12 +14,59 @@ name = 10
 
 
 
-console.log('result' , name + 25) // concatenation
+console.log('result', name + 25) // concatenation
 
-let str ='text'
+let str = 'text'
 
-console.log('result' , str + 25 , 25 + str)
+console.log('result', str + 25, 25 + str)
 
 
-let cars = ['audi',10,true,name,undefined]
+let cars = ['audi', 10, true, name, undefined]
+
+
+function getX(x, y) {
+    console.log('this is function. ', 'Variable: ', x, 'y: ', y)
+}
+getX(10, 5)
+
+function getY(x, y) {
+    //console.log('this is function. ', 'Variable: ', x,'y = ',y)
+    //todo
+    return 'this is function. ' + 'Variable: ' + x, 'y: ' + y
+}
+
+let result = getY('return Y', 'return Y')
+console.log('result: ', result)
+
+if (getY('return Y', 'return Y').length > 5) {
+    // alert('ok')
+}
+// ES6 - web3
+let getZ = () => 'this is short fn'
+console.log(getZ())
+
+let getZ2 = (x, y) => 'this is short fn ' + x + ', y = ' + y
+console.log(getZ2(15, 555))
+
+
+let getZ3 = (x, y) => {
+    console.log('this is function. ', 'x: ', x, ' y: ', y)
+    return 'this is short fn ' + x + ', y = ' + y
+}
+console.log(getZ2(15, 555))
+
+let cars2 = [
+    {
+        title: 'My title',
+        description: 'good title',
+        carnumber: 100,
+        isnewcar: true
+    }
+]
+
+//car = []
+cars2.forEach(element => { //callback
+    console.log('123123: ' + element.title + '|' + element.description)
+
+});
 
